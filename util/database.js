ef5bcf5ddf9c84e2ab0f32be7795ad0e0421be2e
Ian Petty <ipetty@masonite.com>
2021-01-31 23:22:48 -0500

    Bug fix

--- util/database.js
+++ util/database.js
@@ -6,7 +6,8 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://ecommerceRWUser:lTdbvYuW3b6AQ8Vd@cluster0.ghwmn.mongodb.net/shop?retryWrites=true&w=majority'
+    'mongodb+srv://ecommerceRWUser:lTdbvYuW3b6AQ8Vd@cluster0.ghwmn.mongodb.net/shop?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
     .then((client) => {
       console.log('Connected!');
